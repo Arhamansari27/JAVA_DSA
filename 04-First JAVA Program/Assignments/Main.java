1ad0c8b3428476428370652f8ea121563a0aa508
Arham <arhamansari454647@gmail.com>
2024-02-10 21:34:43 +0500

    Lecture-04 First JAVA Program ( Assignment )

--- 04-First JAVA Program/Assignments/Main.java
+++ 04-First JAVA Program/Assignments/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is odd or even
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
