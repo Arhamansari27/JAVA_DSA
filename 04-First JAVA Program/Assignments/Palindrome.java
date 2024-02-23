133ed9d63d44a541abcd9176be97af399da3b263
Arham <arhamansari454647@gmail.com>
2024-02-23 11:47:24 +0500

    7th-question Palindrome

---
+++ 04-First JAVA Program/Assignments/Palindrome.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Palindrome { 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String palindrome = input.next();
+        System.out.println(palindrome);
+    }
+}
