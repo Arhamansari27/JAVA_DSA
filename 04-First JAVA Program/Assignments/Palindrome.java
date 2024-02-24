10da67abd8f16fb4647601563ce961e9d7b8874f
Arham <arhamansari454647@gmail.com>
2024-02-24 10:13:17 +0500

    7th-question Palindrome

--- 04-First JAVA Program/Assignments/Palindrome.java
+++ 04-First JAVA Program/Assignments/Palindrome.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
-public class Palindrome { 
+public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter your String : ");
         String palindrome = input.next();
-        System.out.println(palindrome);
+        StringBuilder strBuilder = new StringBuilder(palindrome);
+        String reversedPalindrome = strBuilder.reverse().toString();
+        if (reversedPalindrome.equals(palindrome)) {
+            System.out.println(reversedPalindrome);
+        }
+        else{
+            System.out.println("Your number is not Palindrome");
+        }
     }
 }
