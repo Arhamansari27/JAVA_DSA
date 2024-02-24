71e789ec0412014489507a034e147fd9fa296491
Arham <arhamansari454647@gmail.com>
2024-02-24 11:08:42 +0500

    Lecture-05-Conditionals and Loops All Notes

--- 04-First JAVA Program/Assignments/Palindrome.java
+++ 04-First JAVA Program/Assignments/Palindrome.java
@@ -1,17 +1,17 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Palindrome {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your String : ");
-        String palindrome = input.next();
-        StringBuilder strBuilder = new StringBuilder(palindrome);
-        String reversedPalindrome = strBuilder.reverse().toString();
-        if (reversedPalindrome.equals(palindrome)) {
-            System.out.println(reversedPalindrome);
-        }
-        else{
-            System.out.println("Your number is not Palindrome");
+    public class Palindrome {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter your String : ");
+            String palindrome = input.next();
+            StringBuilder strBuilder = new StringBuilder(palindrome);
+            String reversedPalindrome = strBuilder.reverse().toString();
+            if (reversedPalindrome.equals(palindrome)) {
+                System.out.println(reversedPalindrome);
+            }
+            else{
+                System.out.println("Your number is not Palindrome");
+            }
         }
     }
-}
