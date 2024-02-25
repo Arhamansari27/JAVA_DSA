876d6eea8f0f832e63479571382f2a77dcfe68b1
Arham <arhamansari454647@gmail.com>
2024-02-25 12:01:18 +0500

    Do while Loop practice

--- 05-Conditionals and Loops/Lecture/Loops.java
+++ 05-Conditionals and Loops/Lecture/Loops.java
@@ -1,27 +1,39 @@
 import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
-        Scanner  input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        // Loops 
+        // Loops
 
         // For Loop
-        // Q-1 Print Number from 1 to n
+
+        // 1-> We use For Loop when we know how many times the loop will execute
 
         // int number = input.nextInt();
         // for (int num = 1; num <= number ; num++){
-        //     System.out.println(num);
+        // System.out.println(num);
         // }
 
-
         // While Loop
+
+        // 1-> We use the While Loop when we don't know how many times the loop will
+        // execute
+
         // System.out.print("Enter the number : ");
         // int number = input.nextInt();
         // System.out.println("Descending order of the number is " );
         // while (number > 0) {
-        //     System.out.println(number);   
-        //     number--;
+        // System.out.println(number);
+        // number--;
         // }
-        
+
+        // Do While Loop
+
+        // 1-> We use Do while Loop when we have to execute the loop at least one time.
+        int n = 1;
+        do {
+            System.out.println("Hello World!");
+        } while (n != 1);
     }
 }
