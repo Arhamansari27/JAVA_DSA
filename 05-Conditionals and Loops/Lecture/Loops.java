bfa5eceade4194a7139b778919b5b8a88635e157
Arham <arhamansari454647@gmail.com>
2024-02-25 11:41:24 +0500

    Lecture-05 While Loop Practice

--- 05-Conditionals and Loops/Lecture/Loops.java
+++ 05-Conditionals and Loops/Lecture/Loops.java
@@ -4,9 +4,24 @@ public class Loops {
         Scanner  input = new Scanner(System.in);
 
         // Loops 
-        int number = input.nextInt();
-        for (int num = 1; num <= number ; num++){
-            System.out.println(num);
-        }
+
+        // For Loop
+        // Q-1 Print Number from 1 to n
+
+        // int number = input.nextInt();
+        // for (int num = 1; num <= number ; num++){
+        //     System.out.println(num);
+        // }
+
+
+        // While Loop
+        // System.out.print("Enter the number : ");
+        // int number = input.nextInt();
+        // System.out.println("Descending order of the number is " );
+        // while (number > 0) {
+        //     System.out.println(number);   
+        //     number--;
+        // }
+        
     }
 }
