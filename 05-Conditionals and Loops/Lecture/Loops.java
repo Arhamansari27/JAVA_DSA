ebbd1d4ca6964cbc9c282554617ec20b662c3857
Arham <arhamansari454647@gmail.com>
2024-02-25 10:53:18 +0500

    Loop Practice

---
+++ 05-Conditionals and Loops/Lecture/Loops.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args) {
+        Scanner  input = new Scanner(System.in);
+
+        // Loops 
+        int number = input.nextInt();
+        for (int num = 1; num <= number ; num++){
+            System.out.println(num);
+        }
+    }
+}
