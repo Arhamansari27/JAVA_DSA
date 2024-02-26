727ef86837ea73fcf14e903eee0f00d9e655d0ba
Arham <arhamansari454647@gmail.com>
2024-02-26 06:37:07 +0500

    Lecture-05 LowerCase/UpperCase

--- 05-Conditionals and Loops/Lecture/TypeChecking.java
+++ 05-Conditionals and Loops/Lecture/TypeChecking.java
@@ -6,8 +6,13 @@ public class TypeChecking {
 
         // A to Z = 65 to 90
         // a to z = 97 to 122
-        System.out.print("Enter the number");
+        System.out.print("Enter the character : ");
         char ch = input.next().trim().charAt(0);
-
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("LowerCase");           
+        }
+        else{
+            System.out.println("UpperCase");
+        }
     }
 }
