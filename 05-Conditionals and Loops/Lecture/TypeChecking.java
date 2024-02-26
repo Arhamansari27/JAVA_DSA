f2e22433ae79a38ca3ef126a3d4637e792ac3a26
Arham <arhamansari454647@gmail.com>
2024-02-26 06:27:53 +0500

    TypeChecking Lecture-05

---
+++ 05-Conditionals and Loops/Lecture/TypeChecking.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TypeChecking {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // A to Z = 65 to 90
+        // a to z = 97 to 122
+        System.out.print("Enter the number");
+        char ch = input.next().trim().charAt(0);
+
+    }
+}
