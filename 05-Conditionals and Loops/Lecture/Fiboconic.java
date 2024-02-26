13f6cc72b5475b272ad08e2dec73e6cbe4b01f1d
Arham <arhamansari454647@gmail.com>
2024-02-26 06:52:56 +0500

    Lecture-05 Fibonacci-Number

---
+++ 05-Conditionals and Loops/Lecture/Fiboconic.java
@@ -0,0 +1,20 @@
+import java.util.*;;
+public class Fiboconic {
+    public static void main(String[] args) {
+        
+        // Fiboconic number is basically the sum of previous two number
+
+        // 0 , 1 , 1 , 2 , 3 , 5 , 8 , 13 ... 
+        Scanner input = new Scanner(System.in);
+
+        int a = 0;
+        int b = 1;
+        int n = input.nextInt();
+        for (int i = 0; i <= n; i++) {
+            int c = a+b;
+            a = b;
+            b =c;
+            System.out.println(c);              
+        }
+    }
+}
