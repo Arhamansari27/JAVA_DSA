cc18f1ed4581da83eb51ff2c7b80937c82327eef
Arham <arhamansari454647@gmail.com>
2024-02-26 07:04:16 +0500

    Lecture-05 Fibonacci-Number

--- 05-Conditionals and Loops/Lecture/Fiboconic.java
+++ 05-Conditionals and Loops/Lecture/Fiboconic.java
@@ -6,15 +6,15 @@ public class Fiboconic {
 
         // 0 , 1 , 1 , 2 , 3 , 5 , 8 , 13 ... 
         Scanner input = new Scanner(System.in);
-
         int a = 0;
         int b = 1;
+        System.out.print("Enter the nth number : ");
         int n = input.nextInt();
         
         for (int i = 0; i < n; i++) {
-            int c = a+b;
+            int c = a + b;
             a = b;
-            b =c;
+            b = c;
             System.out.println(c);              
         }
     }
