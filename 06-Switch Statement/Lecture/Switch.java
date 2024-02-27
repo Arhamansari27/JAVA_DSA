95c1cfb1f8f6cb002737ea516023c84cbe0df9a4
Arham <arhamansari454647@gmail.com>
2024-02-27 14:46:54 +0500

    Lecture-06 Switch Statement Practice

--- 06-Switch Statement/Lecture/Switch.java
+++ 06-Switch Statement/Lecture/Switch.java
@@ -1,6 +1,31 @@
 import java.util.*;
+
 public class Switch {
     public static void main(String[] args) {
-        System.out.println("This is my Lecture of Switch Statement");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the fruit name : ");
+        String str = input.next();
+        switch (str) {
+            case "mango":
+                System.out.println("King of Fruits");
+                break;
+            case "apple":
+                System.out.println("Red Color Fruit");
+                break;
+            case "orange":
+                System.out.println("Rounded Fruit");
+                break;
+            case "banana":
+                System.out.println("Yellow Color Fruit"); 
+                break;
+            default:
+                System.out.println("Enter the valid fruit name and check spell.");
+                break;
+        }
     }
 }
+
+// Week Practice
+// Weekend Practice
+// Year Practice
+// Nested Switch
