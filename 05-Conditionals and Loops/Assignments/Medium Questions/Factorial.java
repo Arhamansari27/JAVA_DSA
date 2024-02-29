e44e00eec00ba9ecf45ea49a012b87dedf75f370
Arham <arhamansari454647@gmail.com>
2024-02-29 14:07:31 +0500

    1st-medium question (Factorial)

---
+++ 05-Conditionals and Loops/Assignments/Medium Questions/Factorial.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String [] args){
+        System.out.println("This is my Practice of Intermediate question.");        
+    }
+}   
+
