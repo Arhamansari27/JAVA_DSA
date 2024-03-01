a4cb12824c68ee92b6818be7f3ecd59047553597
Arham <arhamansari454647@gmail.com>
2024-03-01 07:44:00 +0500

    12-question Perimeter_of_square

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Perimeter_of_square_12.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Perimeter_of_square_12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Perimeter Of Square P = 4a
+        int a = input.nextInt();
+        System.out.println("Perimeter Of Square is " + 4 * a);
+    }
+}
