b02159e89b87574407986ca70cd98cd61f74c1a5
Arham <arhamansari454647@gmail.com>
2024-03-01 07:32:59 +0500

    8th-question Perimeter_of_circle

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Perimeter_of_circle_08.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Perimeter_of_circle_08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // C = 2πr
+        int r = input.nextInt();
+        System.out.println("Perimeter of Circle " + 2 * 3.14159 * r);
+    }
+}
