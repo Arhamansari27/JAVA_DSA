84106be7685acd661a0089bc93a3154ac38f31cb
Arham <arhamansari454647@gmail.com>
2024-03-01 07:19:08 +0500

    5th-question Area_of_paralellogram

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Area_of_paralellogram_05.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Area_of_paralellogram_05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Area Of Parallelogram A = b * h
+
+        int b = input.nextInt();
+        int h = input.nextInt();
+        System.out.println("Area of Parallelogram : " + (b * h));
+    }
+}
