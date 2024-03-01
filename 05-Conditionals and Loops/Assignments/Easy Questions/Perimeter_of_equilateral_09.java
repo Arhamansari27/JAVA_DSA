a895edf329af5ea543744ab40b06f41fe3865d18
Arham <arhamansari454647@gmail.com>
2024-03-01 07:36:02 +0500

    9th-question Perimeter_of_equilateral

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Perimeter_of_equilateral_09.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Perimeter_of_equilateral_09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // P = 3a
+        int a = input.nextInt();
+        System.out.println("Perimeter Of Equilateral Triangle is " + (3*a));
+    }
+}
