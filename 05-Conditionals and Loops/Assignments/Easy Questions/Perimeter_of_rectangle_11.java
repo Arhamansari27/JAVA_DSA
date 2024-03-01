566cfbf2e5f841fb2c81da53932bc8df2600a772
Arham <arhamansari454647@gmail.com>
2024-03-01 07:41:36 +0500

    11-question Perimeter_of_rectangle

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Perimeter_of_rectangle_11.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Perimeter_of_rectangle_11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Perimeter Of Rectangle P = 2(l+w)
+        int l = input.nextInt();
+        int w = input.nextInt();
+        System.out.println("Perimeter Of Rectangle is " + 2 * (l + w));
+    }
+}
