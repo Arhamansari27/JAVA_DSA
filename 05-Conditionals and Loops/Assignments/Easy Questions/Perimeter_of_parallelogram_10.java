ee7f6e986910db286314635461bb709c830dcaf9
Arham <arhamansari454647@gmail.com>
2024-03-01 07:39:10 +0500

    10-question Perimeter_of_parallelogram

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Perimeter_of_parallelogram_10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Perimeter_of_parallelogram_10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Perimeter Of Parallelogram  P = 2(a+b)
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println("Perimeter Of Parallelogram : " + 2*(a + b));
+    }
+}
