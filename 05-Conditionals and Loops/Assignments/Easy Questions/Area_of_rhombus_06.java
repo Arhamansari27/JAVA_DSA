ba09138d5e37f4aebb400d4823251c80fee2e431
Arham <arhamansari454647@gmail.com>
2024-03-01 07:23:09 +0500

    6th-question Area_of_rhombus

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Area_of_rhombus_06.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Area_of_rhombus_06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int p = input.nextInt();
+        int q = input.nextInt();
+        System.out.println("Area of Rhombus is " + (p * q) / 2);
+    }
+}
