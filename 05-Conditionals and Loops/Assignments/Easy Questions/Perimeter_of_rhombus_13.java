dcfe9bccbb1e8f585eb2a1118e1b345c16b20f19
Arham <arhamansari454647@gmail.com>
2024-03-01 07:47:07 +0500

    13-question Perimeter_of_rhombus

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Perimeter_of_rhombus_13.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Perimeter_of_rhombus_13 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println("Perimeter Of Rhombus is " + 4 * a);
+    }
+}
