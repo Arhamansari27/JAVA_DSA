3cb3ebf1c1aeeb652fcab880add5ce151caeba27
Arham <arhamansari454647@gmail.com>
2024-03-01 07:30:10 +0500

    7th-question Area_of_equilateral

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Area_of_equilateral_07.java
@@ -0,0 +1,9 @@
+import java.util.*;;
+
+public class Area_of_equilateral_07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println("Area Of Equilateral Triangle is " + (Math.sqrt(3) / 4) * Math.pow(a, 2));
+    }
+}
