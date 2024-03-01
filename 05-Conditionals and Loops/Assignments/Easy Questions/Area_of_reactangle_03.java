2981c4a1d4511b91e13b34d7e842fcaf51612201
Arham <arhamansari454647@gmail.com>
2024-03-01 07:02:10 +0500

    3rd-question Area_of_rectangle

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Area_of_reactangle_03.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Area_of_reactangle_03 {
+    public static void main(String[] args) {
+        // Area of Rectangle = Lenght * Width
+
+        Scanner input = new Scanner(System.in);
+        int l = input.nextInt();
+        int w = input.nextInt();
+        System.out.println("Area of RectAngle is " + (l*w));
+    }
+}
