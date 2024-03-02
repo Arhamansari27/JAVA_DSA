a4cd47a82f167e20c86d6c27f3a83e2ebe565147
Arham <arhamansari454647@gmail.com>
2024-03-02 10:19:29 +0500

    26th-question Two_sum

---
+++ 05-Conditionals and Loops/Assignments/Easy Questions/Two_sum_26.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Two_sum_26 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int first = in.nextInt();
+        int second = in.nextInt();
+        System.out.println("Addition of 1st number and second number " + (first + second));
+    }
+}
