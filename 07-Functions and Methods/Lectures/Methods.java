bd2430b6237cdbc2d3a4e4f00083f6711730f83f
Arham <arhamansari454647@gmail.com>
2024-03-04 10:52:24 +0500

    Functions Methods Practice

--- 07-Functions and Methods/Lectures/Methods.java
+++ 07-Functions and Methods/Lectures/Methods.java
@@ -1,5 +1,5 @@
 public class Methods {
     public static void main(String[] args) {
-        System.out.println("This is my file of Function Methods ");
+        System.out.println("This is my Practice of Function Methods ");
     }   
 }
