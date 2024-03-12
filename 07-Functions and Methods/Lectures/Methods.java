c66c8e7454c027fda2cfae103bf951fde6520d82
Arham <arhamansari454647@gmail.com>
2024-03-12 14:51:35 +0500

    Greeting Method

--- 07-Functions and Methods/Lectures/Methods.java
+++ 07-Functions and Methods/Lectures/Methods.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Methods {
     public static void main(String[] args) {
-        System.out.println("This is my Practice of Function Methods ");
-    }   
+        // In Java We call Functions as Methods. 
+
+        // Q: Input the 2 numbers and print the sum.
+
+        // This is the simple way but if we want it 10 times the code looks very ugly.
+
+        // Scanner input = new Scanner(System.in);
+        // int num1 = input.nextInt();
+        // int num2 = input.nextInt();
+        // System.out.println(num1 + num2);
+
+
+        greeting();
+    } 
+
+    static void greeting(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Greeting message : ");
+        String greet = in.next();
+        System.out.println(greet);
+    }
 }
