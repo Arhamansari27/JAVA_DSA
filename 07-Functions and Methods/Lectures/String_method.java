c69ebc218fdf395dc358955ba80c59a6b91e7448
Arham <arhamansari454647@gmail.com>
2024-03-14 11:43:45 +0500

    String Method, Parameters , Argumenrs

--- 07-Functions and Methods/Lectures/String_method.java
+++ 07-Functions and Methods/Lectures/String_method.java
@@ -1,10 +1,23 @@
+import java.util.*;
+
 public class String_method {
     public static void main(String[] args) {
-       String msg = greet();
-       System.out.println(msg);
+        // String msg = greet();
+        // System.out.println(msg);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String name = input.next();
+        String newmsg = mygreet(name);
+        System.out.println(newmsg);
     }
+
+    static String mygreet(String name) {
+        String greet = "Hello " + name;
+        return greet;
+    }
+
     static String greet() {
         String greeting = "Hello How are you?";
         return greeting;
     }
-} 
+}
