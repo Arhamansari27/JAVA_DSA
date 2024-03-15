36f09014131326ec78f01fd235d1b48940f06a7e
Arham <arhamansari454647@gmail.com>
2024-03-15 07:38:52 +0500

    Shadowing Method Complete

--- 07-Functions and Methods/Lectures/Shadowing.java
+++ 07-Functions and Methods/Lectures/Shadowing.java
@@ -1,7 +1,20 @@
 import java.util.*;
 
 public class Shadowing {
+    static int x = 100;
+
     public static void main(String[] args) {
-        System.out.println("This is my Shadowing Method.");
+        System.out.println(x);
+
+        int x ; // the class variable at line 4 is shadowed by this
+        x = 50; // Scope will begin when the value is initialize.
+        System.out.println(x);
+
+        fun();
+    }
+
+    static void fun() {
+        // System.out.println(x);
+        System.out.println(x);
     }
 }
