1faedba482440e783ff239dc69cbec8e11e1ee85
Arham <arhamansari454647@gmail.com>
2024-03-16 11:38:22 +0500

    Prime Number Method

--- 07-Functions and Methods/Lectures/Prime_method.java
+++ 07-Functions and Methods/Lectures/Prime_method.java
@@ -2,20 +2,25 @@ import java.util.*;
 
 public class Prime_method {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        boolean primenumber = true;
-
-        for (int i = 2; i < a; i++) {
-            if (a % i == 0) {
-                primenumber = false;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int ans = prime(input.nextInt());
+    }
+    static int prime(int nums){
+        int nowprime = nums;
+        boolean isprime = true;
+        for (int i = 2; i < nowprime; i++) {
+            if (nowprime % i == 0) {
+                isprime = false;
                 break;
             }
         }
-        if (primenumber) {
-            System.out.println(a + " is a prime number");
-        } else {
-            System.out.println(a + " is not a prime Number");
+        if (isprime) {
+            System.out.println(nowprime + " is a prime Number");
         }
+        else{
+            System.out.println(nowprime + " is not a prime number");
+        }
+        return nowprime;
     }
 }
