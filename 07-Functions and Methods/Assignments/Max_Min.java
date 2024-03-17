3c428f054b7b5f99a7148b6ce6024b59d901b90c
Arham <arhamansari454647@gmail.com>
2024-03-17 06:12:39 +0500

    Max_Min Method Question-01

--- 07-Functions and Methods/Assignments/Max_Min.java
+++ 07-Functions and Methods/Assignments/Max_Min.java
@@ -1,6 +1,25 @@
 import java.util.*;
 public class Max_Min{
     public static void main(String[] args) {
-        System.out.println("Assignments Start Now");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the 1st number : ");
+        int fst = input.nextInt();
+        System.out.print("Enter the 2nd number : ");
+        int scnd = input.nextInt();
+        System.out.print("Enter the 3rd number : ");
+        int trd = input.nextInt();
+
+        int ans = max_min(fst , scnd , trd);
+        System.out.println(ans);
+    }
+    static int max_min(int first , int second , int third){
+        int max = first;
+        if (second > max) {
+            max = second;
+        }
+         if (third > max) {
+            max = third;
+        }
+        return max;
     }
 }
