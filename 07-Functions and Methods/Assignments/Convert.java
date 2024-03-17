16023e24f4b68692ca1df606ef4d9543ea36f6be
Arham <arhamansari454647@gmail.com>
2024-03-17 07:20:10 +0500

    Convert-Programs Question-11

--- 07-Functions and Methods/Assignments/Convert.java
+++ 07-Functions and Methods/Assignments/Convert.java
@@ -1,5 +1,5 @@
 public class Convert {
     public static void main(String[] args) {
-        System.out.println("I almost done all the Assignments before.");
+        System.out.println("I almost done my all Assignments before.");
     }
 }
