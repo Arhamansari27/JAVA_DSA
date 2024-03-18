8241af5ec599948c65388d9fa3e86d9e26368c52
Arham <arhamansari454647@gmail.com>
2024-03-18 06:28:19 +0500

    Medium Question-01

--- 05-Conditionals and Loops/Assignments/Medium Questions/Factorial.java
+++ 05-Conditionals and Loops/Assignments/Medium Questions/Factorial.java
@@ -2,13 +2,6 @@ import java.util.*;
 
 public class Factorial {
     public static void main(String[] args) {
-        int a = 5;
-        int fact = 1;
-        int mult = 1;
-        while (mult < a) {
-            mult++;
-            fact = fact * mult;
-        }
-        System.out.println(fact);
+     System.out.println("This is 1st Medium Quesion.");
     }
 }
