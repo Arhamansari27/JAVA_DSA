32ec116438fefb34d9bb5c24607d51445707117c
Arham <arhamansari454647@gmail.com>
2024-03-19 10:02:15 +0500

    Average Question-03

---
+++ 05-Conditionals and Loops/Assignments/Medium Questions/Average.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Average {
+    public static void main(String[] args) {
+
+        // To calculate the average of N numbers, you follow these steps:
+
+        // Add up all the numbers: Sum all the given numbers together.
+        // Count the numbers: Determine how many numbers you have (N).
+        // Divide the sum by the count: Divide the sum obtained in step 1 by the count
+        // obtained in step 2.
+        // The formula for average is:
+
+        // Average = Sum of all numbers / Count of numbers
+
+      System.out.println("Average Assignment Start");
+    }
+}
