7c7264ff5edb71ab73dfc3aa803a6db6021d77b0
Arham <arhamansari454647@gmail.com>
2024-03-20 11:10:33 +0500

    NCR Questiom-15

---
+++ 05-Conditionals and Loops/Assignments/Medium Questions/ncr_npr.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ncr_npr {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        int n = in.nextInt();
+        System.out.print("Enter the value of r : ");
+        int r = in.nextInt();
+
+        int npdr = 1;
+        int rpdr = 1;
+        int newpd = 1;
+
+        // nCr = n! / r! * (n-r)!
+
+        for (int i = 1; i <= n; i++) {
+            npdr = npdr * i;
+        }
+        for (int i = 1; i <= r; i++) {
+            rpdr = rpdr * i;
+        }
+
+        int subtraction = n - r;
+
+        for (int i = 1; i <= subtraction; i++) {
+            newpd = newpd * i;
+        }
+
+        int ans = npdr / (rpdr * newpd);
+        System.out.println( "Your answer is "+ ans);
+    }
+}
