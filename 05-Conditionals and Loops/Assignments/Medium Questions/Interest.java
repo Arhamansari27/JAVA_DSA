fb18147295bc37b383f593bbf79e2a2bc6f6876c
Arham <arhamansari454647@gmail.com>
2024-03-20 10:25:37 +0500

    Interest Question-11

--- 05-Conditionals and Loops/Assignments/Medium Questions/Interest.java
+++ 05-Conditionals and Loops/Assignments/Medium Questions/Interest.java
@@ -1,6 +1,20 @@
 import java.util.*;
 public class Interest{
     public static void main(String[] args) { 
-        System.out.println("Done");
+
+        // A = P(1 + r/n)^(nt)
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the value of P : ");
+        double p = in.nextDouble();
+        System.out.print("Enter the value of r : ");
+        double r = in.nextDouble() / 100;
+        System.out.print("Enter the value of n : ");
+        double n = in.nextDouble();
+        System.out.print("Enter the value of t : ");
+        double t = in.nextDouble();
+
+        double ans = p * Math.pow((1 + r/n) , (n*t));
+        System.out.println("Your Compound Interest is : " + ans);
     }
 }
