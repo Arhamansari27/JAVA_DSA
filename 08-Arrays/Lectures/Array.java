a070e6e16b2b89c994b71ef168529631de990a37
Arham <arhamansari454647@gmail.com>
2024-03-21 10:19:15 +0500

    Lecture-08 Array

---
+++ 08-Arrays/Lectures/Array.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Array{
+ public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("This is my practice of Array.");
+ }   
+}
