61a3d96c3abb47f19ebd2ec6c447131af926d511
Arham <arhamansari454647@gmail.com>
2024-03-23 10:29:20 +0500

    Two-Dimensional Array

---
+++ 08-Arrays/Lectures/twod_Array.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class twod_Array {
+    public static void main(String[] args) {
+        // Two D Array means Array in Array
+
+        // int[][] twod = {
+        //         { 1, 2, 3 },
+        //         { 4, 5, },
+        //         { 6, 7, 8, 9 }
+        // };
+
+        // System.out.println(Arrays.toString(twod[2]));
+
+        // Printing 2D array
+
+        // for (int i = 0; i < twod.length; i++) {
+        //     System.out.println(Arrays.toString(twod[i]));
+        // }
+    }
+}
