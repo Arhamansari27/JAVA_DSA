c0373ec31e0de7b449618e49d46f6e182b2cba57
Arham <arhamansari454647@gmail.com>
2024-03-23 06:25:29 +0500

    Lecture-08 Array

--- 08-Arrays/Lectures/Array.java
+++ 08-Arrays/Lectures/Array.java
@@ -1,7 +1,10 @@
 import java.util.*;
-public class Array{
- public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    System.out.println("This is my practice of Array.");
- }   
+
+public class Array {
+    public static void main(String[] args) {
+
+         int [] arr; //  => Indicates the data-type of the array
+         arr = new int[5]; // => Initialization and object being created in the heap memory
+         System.out.println("Print");
+    }
 }
