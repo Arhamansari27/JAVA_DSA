c2a4b8a7b1864621e1b74023edd472225ba33830
Arham <arhamansari454647@gmail.com>
2024-03-27 08:59:13 +0500

    2d Array Col not fixed

---
+++ 08-Array/Lectures/Col.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Col {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[][] arr = {
+                { 1, 2, 3, 4, 5 },
+                { 6, 7 },
+                { 8, 9, 10 }
+        };
+
+        System.out.println("Enter the Array: ");
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
