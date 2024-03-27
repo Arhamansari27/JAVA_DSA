8a4c892c008ef3500d48d3e5eacfb8ba86335bc4
Arham <arhamansari454647@gmail.com>
2024-03-27 21:24:24 +0500

    Array List Almost All done

--- 08-Array/Lectures/Array_List.java
+++ 08-Array/Lectures/Array_List.java
@@ -1,31 +1,43 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Array_List {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         ArrayList<Integer> list =  new ArrayList<>();
         
         // Add for add element
         // Set for update element
         // Remove for remove element
 
-        list.add(12); 
-        list.add(13);
-        list.add(14);
-        list.add(15);
-        list.add(16);
-        list.add(17);
-        list.add(18);
-        list.add(19);
+        // list.add(12); 
+        // list.add(13);
+        // list.add(14);
+        // list.add(15); 
+        // list.add(16);
+        // list.add(17);
+        // list.add(18);
+        // list.add(19);
         
-        System.out.print(list);
+        // System.out.print(list);
 
-        list.set(0, 191);
-        System.out.println();
-        System.out.print(list);
+        // list.set(0, 191);
+        // System.out.println();
+        // System.out.print(list);
 
-        list.remove(0);
-        System.out.println();
-        System.out.println(list);
+        // list.remove(0);
+        // System.out.println();
+        // System.out.println(list);
+
+        // Input the List and Output the List
+        
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        
     }
 
 }
