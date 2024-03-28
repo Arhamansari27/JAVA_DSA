c29f818157248663dd6bf811c79a18a593fbf2b1
Arham <arhamansari454647@gmail.com>
2024-03-28 10:29:49 +0500

    Array List Almost Done

--- 08-Array/Lectures/Array_List.java
+++ 08-Array/Lectures/Array_List.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 public class Array_List {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList<Integer> list =  new ArrayList<>();
-        
-        // Add for add element
-        // Set for update element
-        // Remove for remove element
+        ArrayList<Integer> list = new ArrayList<>();
 
-        // list.add(12); 
-        // list.add(13);
-        // list.add(14);
-        // list.add(15); 
-        // list.add(16);
-        // list.add(17);
-        // list.add(18);
-        // list.add(19);
-        
+        // Add for add element.
+        // Set for update element.
+        // Remove for remove element.
+        // Contain means element that contain.
+
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(15);
+        list.add(16);
+        list.add(17);
+        list.add(18);
+        list.add(19);
+
         // System.out.print(list);
 
         // list.set(0, 191);
@@ -30,14 +31,14 @@ public class Array_List {
         // System.out.println(list);
 
         // Input the List and Output the List
-        
+
         for (int i = 0; i < 5; i++) {
             list.add(in.nextInt());
         }
         for (int i = 0; i < 5; i++) {
             System.out.print(list.get(i) + " ");
         }
-        
+
     }
 
 }
