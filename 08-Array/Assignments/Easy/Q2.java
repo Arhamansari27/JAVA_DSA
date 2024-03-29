2e4e3943b4ab668bda794d35a74b63210333fdcb
Arham <arhamansari454647@gmail.com>
2024-03-29 10:42:34 +0500

    Q2 Leetcode-Question

---
+++ 08-Array/Assignments/Easy/Q2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Q2{
+    public static void main(String[] args) {
+        int nums[] = {1 ,2 ,1};
+
+        int len = nums.length;
+
+        int result [] = new int [len * 2];
+
+        for (int i = 0; i <result.length; i++) {
+          int ans = nums[i];
+        }
+    }
+}
