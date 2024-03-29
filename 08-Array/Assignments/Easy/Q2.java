679a250bfb85e064b7b7f04501e8b09618f75c37
Arham <arhamansari454647@gmail.com>
2024-03-29 15:18:17 +0500

    Q2 Leetcode Question Done

--- 08-Array/Assignments/Easy/Q2.java
+++ 08-Array/Assignments/Easy/Q2.java
@@ -1,14 +1,28 @@
 import java.util.*;
-public class Q2{
-    public static void main(String[] args) {
-        int nums[] = {1 ,2 ,1};
 
-        int len = nums.length;
+public class Q2 {
+  public static void main(String[] args) {
+    int nums[] = { 1, 2, 1 };
 
-        int result [] = new int [len * 2];
+    int len = nums.length;
 
-        for (int i = 0; i <result.length; i++) {
-          int ans = nums[i];
-        }
+    int result[] = new int[len * 2];
+
+    for (int i = 0; i < nums.length; i++) {
+      result[i] = nums[i];
+      result[i + len] = nums[i];
+      System.out.println(result[i]);
     }
+  }
 }
+
+
+// LeetCode Code
+
+// int[] ans = new int[2*nums.length];
+
+// for(int i=0;i<nums.length;i++) {
+//     ans[i] = nums[i];
+//     ans[i+nums.length] = nums[i];
+// }
+// return ans;
