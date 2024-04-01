3bb1065695da17bae6aafe8933273c92bd48d8c2
Arham <arhamansari454647@gmail.com>
2024-04-01 09:56:36 +0500

    Question-06 LeetCode

--- 08-Array/Assignments/Easy/Q6.java
+++ 08-Array/Assignments/Easy/Q6.java
@@ -12,3 +12,17 @@ public class Q6 {
     }
   }
 }
+
+// LeetCode Solution
+
+// class Solution {
+//   public int[] shuffle(int[] nums, int n) {
+//       int len = nums.length;
+//       int [] ans = new int [len];
+//       for(int i = 0; i < n; i++){
+//           ans[2*i] = nums[i];
+//           ans[2*i + 1] = nums[i + n];
+//       }
+//       return ans;
+//   }
+// }
