d95afe674915b25c77c61bff189d1fa83eca3c96
Arham <arhamansari454647@gmail.com>
2024-04-01 09:51:07 +0500

    Question-06 LeetCode

--- 08-Array/Assignments/Easy/Q6.java
+++ 08-Array/Assignments/Easy/Q6.java
@@ -1,5 +1,14 @@
 import java.util.*;
-public class Q6{
- public static void main(String[] args) {
- }   
+
+public class Q6 {
+  public static void main(String[] args) {
+    int[] arr = { 2, 5, 1, 3, 4, 7 };
+    int n = 3;
+    int len = arr.length;
+    int [] ans = new int[len];
+    for (int i = 0; i < len; i++) {
+      ans[2 * i] = arr[i];
+      ans[2 *i + 1] = arr[i+n];
+    }
+  }
 }
