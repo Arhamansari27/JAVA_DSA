e67b5e2a9f278abccf6d4c7e3204a62bc00a0fdc
Arham <arhamansari454647@gmail.com>
2024-04-02 10:50:39 +0500

    Question-09

--- 08-Array/Assignments/Easy/Q9.java
+++ 08-Array/Assignments/Easy/Q9.java
@@ -7,10 +7,22 @@ public class Q9 {
         int [] result = new int [len];
 
         for (int i = 0; i < nums.length ; i++) {
-            // System.out.print(nums[i] + " ");
-            // System.out.print(indexes[i] + " ");
             result[i] = nums[indexes[i]];
             System.out.println(result[i]);
         }
+    }
+}
+
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int len = nums.length;
+        int [] ans = new int [len];
+        for(int i = 0; i < len; i++){
+            for (int j = len - 1; j > index[i]; j--) {
+                ans[j] = ans[j - 1];
+            }
+            ans[index[i]] = nums[i];
+        }
+        return ans; 
     }
 }
