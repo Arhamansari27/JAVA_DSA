823dafee9d2161aa524339c8f48713a294105a84
Arham <arhamansari454647@gmail.com>
2024-04-02 09:40:03 +0500

    Question-08 LeetCode

---
+++ 08-Array/Assignments/Easy/Q8.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Q8{
+    public static void main(String[] args) {
+        int [] nums = { 1,2,3} ;
+        int count = 0;
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+// LeetCode Solution
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0 ;
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
