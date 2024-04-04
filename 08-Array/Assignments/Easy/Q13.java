17d7930e3cdf74561748016ee5f8b5a838e4f1ca
Arham <arhamansari454647@gmail.com>
2024-04-04 09:39:52 +0500

    Questions Readme File

---
+++ 08-Array/Assignments/Easy/Q13.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Q13 {
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 2 };
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
