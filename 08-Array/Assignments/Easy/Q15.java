f089c553d2e4b6aca1f50a795586e1c608c23477
Arham <arhamansari454647@gmail.com>
2024-04-07 09:28:44 +0500

    Question-15

--- 08-Array/Assignments/Easy/Q15.java
+++ 08-Array/Assignments/Easy/Q15.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Q15 {
@@ -7,7 +6,7 @@ public class Q15 {
                 { 1, 2, 3 },
                 { 4, 5, 6 },
                 { 7, 8, 9 }
-        };
+        }; 
 
         int len = matrix.length;
 
@@ -20,3 +19,21 @@ public class Q15 {
         }
     }
 }
+
+// LeetCode Solution
+
+// class Solution {
+//     public int[][] transpose(int[][] matrix) {
+//         int n = matrix.length;
+//         int m = matrix[0].length;
+
+//         int[][] ans = new int[m][n];
+
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < m; j++){
+//                 ans[j][i] = matrix[i][j];
+//             }
+//         }
+//         return ans;
+//     }
+// }
