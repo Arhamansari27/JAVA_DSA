85a6546e43325d64fc7354535e36c280776385aa
Arham <arhamansari454647@gmail.com>
2024-04-07 06:32:27 +0500

    Two-Dimensional-Array

--- 08-Array/Lectures/Two_dArray.java
+++ 08-Array/Lectures/Two_dArray.java
@@ -1,6 +1,24 @@
 import java.util.*;
-public class Two_dArray{
+
+public class Two_dArray {
     public static void main(String[] args) {
-        
+         Scanner sc = new Scanner(System.in);
+         int row = sc.nextInt();
+         int col = sc.nextInt();
+
+         int [][] twod = new int [row][col];
+
+         for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                twod[i][j] = sc.nextInt();
+            }
+         }
+
+         for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(twod[i][j] + " ");
+            }
+            System.out.println();
+         }
     }
 }
