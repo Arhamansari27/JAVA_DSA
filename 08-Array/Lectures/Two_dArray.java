471f1793a5b800e687467181ace4f406dd6fbc55
Arham <arhamansari454647@gmail.com>
2024-04-07 10:28:45 +0500

    Two-Dimensional-Array

--- 08-Array/Lectures/Two_dArray.java
+++ 08-Array/Lectures/Two_dArray.java
@@ -2,23 +2,51 @@ import java.util.*;
 
 public class Two_dArray {
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-         int row = sc.nextInt();
-         int col = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
 
-         int [][] twod = new int [row][col];
+        // One Method
 
-         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                twod[i][j] = sc.nextInt();
-            }
-         }
+        // int row = sc.nextInt();
+        // int col = sc.nextInt();
 
-         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print(twod[i][j] + " ");
-            }
-            System.out.println();
-         }
+        // int[][] twod = new int[row][col];
+
+        // for (int i = 0; i < row; i++) {
+        // for (int j = 0; j < col; j++) {
+        // twod[i][j] = sc.nextInt();
+        // System.out.print(twod[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
+
+        int[][] newarr = {
+                { 1, 2, 3, 6 , 7 ,8 ,9},
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+
+        // for (int i = 0; i < newarr.length; i++) {
+        //     for (int j = 0; j < newarr[i].length; j++) {
+        //         System.out.print(newarr[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // For each Loop
+
+        // for (int[] iterate : newarr) {
+        //     System.out.println(Arrays.toString(iterate));
+        // }
+
+        String [][] stringarr = {
+            { "Muhammad"},
+            {"Arham"},
+            {"Ansari"}
+        };
+
+        for (String[] arrraystrings : stringarr) {
+            System.out.println(Arrays.toString(arrraystrings));
+        }
     }
 }
