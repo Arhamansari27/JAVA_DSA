41a06041e5153d36723b464ade7fca49b14ca74a
Arham <arhamansari454647@gmail.com>
2024-04-17 11:56:20 +0500

    Array Lists

--- 08-Arrays/Lectures/Array_List.java
+++ 08-Arrays/Lectures/Array_List.java
@@ -1,44 +1,47 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Array_List {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
-
-        // Add for add element.
-        // Set for update element.
-        // Remove for remove element.
-        // Contain means element that contain.
-
-        list.add(12);
-        list.add(13);
-        list.add(14);
-        list.add(15);
-        list.add(16);
-        list.add(17);
-        list.add(18);
-        list.add(19);
-
-        // System.out.print(list);
-
-        // list.set(0, 191);
-        // System.out.println();
-        // System.out.print(list);
-
-        // list.remove(0);
-        // System.out.println();
-        // System.out.println(list);
-
-        // Input the List and Output the List
-
-        for (int i = 0; i < 5; i++) {
-            list.add(in.nextInt());
-        }
-        for (int i = 0; i < 5; i++) {
-            System.out.print(list.get(i) + " ");
-        }
 
     }
-
 }
+
+
+
+
+
+// Scanner in = new Scanner(System.in);
+// ArrayList<Integer> list = new ArrayList<>();
+
+// // Add for add element.
+// // Set for update element.
+// // Remove for remove element.
+// // Contain means element that contain.
+
+// list.add(12);
+// list.add(13);
+// list.add(14);
+// list.add(15);
+// list.add(16);
+// list.add(17);
+// list.add(18);
+// list.add(19);
+
+// // System.out.print(list);
+
+// // list.set(0, 191);
+// // System.out.println();
+// // System.out.print(list);
+
+// // list.remove(0);
+// // System.out.println();
+// // System.out.println(list);
+
+// // Input the List and Output the List
+
+// for (int i = 0; i < 5; i++) {
+//     list.add(in.nextInt());
+// }
+// for (int i = 0; i < 5; i++) {
+//     System.out.print(list.get(i) + " ");
+// }
